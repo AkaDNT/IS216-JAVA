0aaa17695f21b890e5090dfef05ce26816d8e1b9
AkaDNT <nhatthong346@gmail.com>
2025-05-27 21:08:42 +0700

    feat(backend): Add place order service

---
+++ Backend/Bookstore/Bookstore/src/main/java/com/rgbunny/dao/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.rgbunny.dao;
+
+import com.rgbunny.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
